d8bb254162801d9cb40c265f075ff7215a5f49de
Steve Swinsburg <steve.swinsburg@gmail.com>
2012-02-14 01:38:32 +0000

    PORt-717 add adapter for wikispaces
    git-svn-id: https://doiweb-repo.anu.edu.au/repositories/es-projects/anu-portlets/basiclti-portlet/trunk@11584 37c5b581-7024-0410-98b2-893b731772de

---
+++ src/main/java/au/edu/anu/portal/portlets/basiclti/adapters/WikispacesAdapter.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright 2010-2011 The Australian National University
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package au.edu.anu.portal.portlets.basiclti.adapters;
+
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+
+/**
+ * An adapter for connecting to Wikispaces (www.wikispaces.com)
+ * 
+ * @author Steve Swinsburg (steve.swinsburg@anu.edu.au)
+ *
+ */
+public class WikispacesAdapter extends AbstractAdapter {
+
+	private final Log log = LogFactory.getLog(getClass().getName());
+
+	
+	/**
+	 * Currently returns the map of params unchanged (except for adding the default params).
+	 * 
+	 * <p>
+	 * Note this is currently equivalent to a standard Basic LTI launch.
+	 * This adapter has been added to cater for any custom behaviour that Wikispaces may provide in the future.
+	 * 
+	 * @param params	map of launch data params
+	 * @return the map, unchanged
+	 */
+	@Override
+	public Map<String,String> processLaunchData(Map<String,String> params){
+		
+		log.debug("WikispacesAdapter.processLaunchData() called");
+
+		//add defaults
+		params.putAll(super.getDefaultParameters());
+		
+		return params;
+	}
+	
+	
+}
