09c524786810942b816fb5becb954e54c330c2f1
Steve Swinsburg <steve.swinsburg@gmail.com>
2012-04-24 01:48:20 +0000

    PORT-883 update license headers
    git-svn-id: https://doiweb-repo.anu.edu.au/repositories/es-projects/anu-portlets/basiclti-portlet/trunk@11897 37c5b581-7024-0410-98b2-893b731772de

--- src/main/java/au/edu/anu/portal/portlets/basiclti/adapters/BasicLTIAdapterFactory.java
+++ src/main/java/au/edu/anu/portal/portlets/basiclti/adapters/BasicLTIAdapterFactory.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2010-2011 The Australian National University
+ * Copyright 2010-2012 The Australian National University
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-
 package au.edu.anu.portal.portlets.basiclti.adapters;
 
 import org.apache.commons.logging.Log;
